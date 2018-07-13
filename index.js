f845b3ee30228f5413c008ffbc773ebf8f2582a8
jarretbryan <jarret.bryan@gmail.com>
2018-07-13 19:27:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+
+const currentUser = "Jarret Bryan";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
